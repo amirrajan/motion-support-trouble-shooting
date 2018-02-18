01931d129b93018f43dcd38942b66f0e5ddabe2c
Amir Rajan <ar@amirrajan.net>
2018-02-18 14:51:08 -0600

    Integer, Inflector, Kernel consolidatation.

--- app/lib/integer.rb
+++ app/lib/integer.rb
@@ -1,4 +1,39 @@
 class Integer
+  # # Ordinalize turns a number into an ordinal string used to denote the
+  # # position in an ordered sequence such as 1st, 2nd, 3rd, 4th.
+  # #
+  # #  1.ordinalize     # => "1st"
+  # #  2.ordinalize     # => "2nd"
+  # #  1002.ordinalize  # => "1002nd"
+  # #  1003.ordinalize  # => "1003rd"
+  # #  -11.ordinalize   # => "-11th"
+  # #  -1001.ordinalize # => "-1001st"
+  # def ordinalize
+  #   MotionSupport::Inflector.ordinalize(self)
+  # end
+
+  # # Ordinal returns the suffix used to denote the position
+  # # in an ordered sequence such as 1st, 2nd, 3rd, 4th.
+  # #
+  # #  1.ordinal     # => "st"
+  # #  2.ordinal     # => "nd"
+  # #  1002.ordinal  # => "nd"
+  # #  1003.ordinal  # => "rd"
+  # #  -11.ordinal   # => "th"
+  # #  -1001.ordinal # => "st"
+  # def ordinal
+  #   MotionSupport::Inflector.ordinal(self)
+  # end
+
+  # # Check whether the integer is evenly divisible by the argument.
+  # #
+  # #   0.multiple_of?(0)  #=> true
+  # #   6.multiple_of?(5)  #=> false
+  # #   10.multiple_of?(2) #=> true
+  # def multiple_of?(number)
+  #   number != 0 ? self % number == 0 : zero?
+  # end
+
   # # Enables the use of time calculations and declarations, like <tt>45.minutes +
   # # 2.hours + 4.years</tt>.
   # #
